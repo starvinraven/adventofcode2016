072ca3c7b8953c83447cc6735a4d579c03754f51
Esa Virtanen <esa@everholt.org>
2016-12-19 18:02:00 +0200

    day13

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/math.combinatorics "0.1.3"]
-                 [digest "1.4.5"]])
+                 [digest "1.4.5"]
+                 [aysylu/loom "0.6.0"]])
---
+++ src/adventofcode2016/day_13.clj
@@ -0,0 +1,42 @@
+(ns adventofcode2016.day-13
+    (:require [loom.alg-generic :refer [dijkstra-path bf-traverse]]))
+
+(def input 1364)
+(def start-node [1 1])
+(def end-node [31 39])
+
+(defn is-wall?
+      [x y]
+      (let [str (Long/toBinaryString (+ (* x x) (* 3 x) (* 2 x y) (* y y) y input))
+            num-ones (count (filter (fn [x] (= x \1)) str))
+            even? (= 0 (mod num-ones 2))]
+           (not even?)))
+
+(defn neighbors
+      [[x y]]
+      (filter (fn [[xx yy]]
+                  (and (not (is-wall? xx yy))
+                       (>= xx 0)
+                       (>= yy 0)))
+              [[(inc x) y]
+               [(dec x) y]
+               [x (inc y)]
+               [x (dec y)]]))
+
+(defn run-pt1
+      []
+      (let [path (dijkstra-path
+                   neighbors
+                   (constantly 1)
+                   start-node
+                   end-node)]
+           (println (dec (count path)))
+           path))
+
+(defn run-pt2
+      []
+      (count
+        (bf-traverse
+          neighbors
+          start-node
+          :when (fn [_ _ depth] (<= depth 50)))))
